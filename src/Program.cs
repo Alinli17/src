27ac1a3c923cb21a02e541f321a47ff33efc145c
stu-pksp117 <stu-pksp117@AMDVLA3511GW06>
2021-04-19 12:45:56 +0300

    Сделала проверку на ввод размера массива

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO; 
 
 namespace src
 {
     class Program
     {
+        private static int n;
+        private static Trip[] Trips;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                string n1 = Console.ReadLine();
+
+                while (!Int32.TryParse(n1, out n)|| n < 0) 
+                {
+                    Console.WriteLine("Введите целое положительное число!");
+                    Console.Write("Введите размер массива: ");
+                    n1 = Console.ReadLine();
+                }
+                Trips = new Trip[n];
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
         }
     }
 }
