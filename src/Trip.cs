81a9cb5eebb8571b6c785ebb283b48070e3850d9
stu-pksp117 <stu-pksp117@AMDVLA3511GW06>
2021-04-19 12:33:50 +0300

    Описала поля класска и методы для них

--- src/Trip.cs
+++ src/Trip.cs
@@ -6,5 +6,25 @@ namespace src
 {
     class Trip
     {
+        string duration = "";
+        int price = 0;
+        string number_of_people = "";
+
+
+        public string Duration
+        {
+            get { return this.duration; }
+            set { this.duration = value; }
+        }
+        public string Number
+        {
+            get { return this.number_of_people; }
+            set { this.number_of_people = value; }
+        }
+        public int Price 
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
     }
 }
